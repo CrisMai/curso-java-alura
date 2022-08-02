7d1cb4d2002a00e152d13b924da38de4ff225795
CrisMai <crismai.dev@gmail.com>
2022-08-02 14:21:37 -0300

    Condicional boolean

---
+++ src/Condicional2.java
@@ -0,0 +1,16 @@
+
+public class Condicional2 {
+
+	public static void main(String[] args) {
+		int idade = 20;
+		boolean acompanhado = true;
+
+		if (idade >= 18 && acompanhado) {
+			System.out.println("Seja bem vindo!");
+		} else {
+
+			System.out.println("Infelizmente você não pode entrar.");
+		}
+	}
+
+}
