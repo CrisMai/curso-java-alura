26b76336f6e38dd877d9c70971f057921b37a255
CrisMai <crismai.dev@gmail.com>
2022-08-03 16:49:23 -0300

    Método com retorno

--- src/bytebank/Metodo.java
+++ src/bytebank/Metodo.java
@@ -1,14 +1,18 @@
 package bytebank;
 
 public class Metodo {
-	
+
 	public static void main(String[] args) {
-		
+
 		Conta contaDaCris = new Conta();
 		contaDaCris.saldo = 100;
 		contaDaCris.deposita(50);
-		
+
 		System.out.println(contaDaCris.saldo);
+
+		boolean conseguiuRetirar = contaDaCris.saca(20);
+		System.out.println(contaDaCris.saldo);
+		System.out.println(conseguiuRetirar);
 	}
 
 }
