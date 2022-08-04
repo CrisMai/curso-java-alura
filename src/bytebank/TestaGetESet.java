b1fd72391c3125cf7b9644eeeb92c2c0db77bda3
CrisMai <crismai.dev@gmail.com>
2022-08-04 15:21:06 -0300

    Getters e setters de referência

--- src/bytebank/TestaGetESet.java
+++ src/bytebank/TestaGetESet.java
@@ -6,6 +6,13 @@ public class TestaGetESet {
 		Conta conta = new Conta();
 		conta.setNumero(1334);
 		System.out.println(conta.getNumero());
+		
+		Cliente cris = new Cliente();
+		cris.setNome("Cris Mai");
+		
+		
+		conta.setTitular(cris);
+		System.out.println(conta.getTitular().getNome());
 	}
 
 }
