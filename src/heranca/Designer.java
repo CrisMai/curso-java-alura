f669716e62cd1674956cea3840f3aa84a26a2bfd
CrisMai <crismai.dev@gmail.com>
2022-08-08 15:06:24 -0300

    Aplicando Polimorfismo

---
+++ src/heranca/Designer.java
@@ -0,0 +1,10 @@
+package heranca;
+
+public class Designer extends Funcionario {
+	
+	public double getBonificacao() {
+		System.out.println("Chamando o método de bonificação do designer");
+		return 200;
+	}
+
+}
