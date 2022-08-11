d43ced36edd3bfbd957837465d06511e183524d7
CrisMai <crismai.dev@gmail.com>
2022-08-11 16:14:20 -0300

    Try com recursos

--- src/javaexcecoes/TesteConexao.java
+++ src/javaexcecoes/TesteConexao.java
@@ -1,22 +1,39 @@
 package javaexcecoes;
 
 public class TesteConexao {
-	
+
 	public static void main(String[] args) {
+
 		
-		Conexao con = null;
-		
-		try {
-			con = new Conexao();
-			con.leDados();
+		try (Conexao conexao = new Conexao()) {
+			conexao.leDados();	
 			
 		} catch (IllegalStateException ex) {
-			System.out.println("Deu erro na conexao");
-			
-		} finally {
-			con.fecha();
+			System.out.println("Deu erro na conexao");	
 		}
 		
+		
+		
+		
+		//--------------------------
+		
+//		Conexao con = null;
+
+//		try {
+//			con = new Conexao();
+//			con.leDados();
+//		} catch (IllegalStateException ex) {
+//			
+//			System.out.println("Deu erro na conexao");
+//			
+//		} finally {
+//			System.out.println("finally");
+//			if (con != null) {
+//				con.close();
+//			}
+//
+//		}
+
 	}
 
 }
