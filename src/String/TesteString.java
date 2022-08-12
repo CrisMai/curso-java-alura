e344a866f8a3ace1ae8a13be5e252e2488bf6e5c
CrisMai <crismai.dev@gmail.com>
2022-08-12 15:54:06 -0300

    Classe String

---
+++ src/String/TesteString.java
@@ -0,0 +1,15 @@
+package String;
+
+public class TesteString {
+
+	public static void main(String[] args) {
+
+		String nome = "Alura"; // Object literal
+		// String outro = new String("Alura");
+
+		String outra = nome.replace("A", "a");
+
+		System.out.println(nome);
+		System.out.println(outra);
+	}
+}
