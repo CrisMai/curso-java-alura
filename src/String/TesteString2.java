5d1aafe50578cddf6fd34f9e843d387a92c0146c
CrisMai <crismai.dev@gmail.com>
2022-08-12 16:21:45 -0300

    Métodos da classe String

---
+++ src/String/TesteString2.java
@@ -0,0 +1,27 @@
+package String;
+
+public class TesteString2 {
+
+	public static void main(String[] args) {
+
+		String nome = "Alura";
+
+		// char c = nome.charAt(2); // Método charAt().
+		// System.out.println(c);
+
+		// int pos = nome.indexOf("ur"); // Método indexOf().
+		// System.out.println(pos);
+
+		// String sub = nome.substring(1); // Método substring().
+		// System.out.println(sub);
+
+		// String vazio = ""; // Método isEmpty().
+		// System.out.println(vazio.isEmpty();
+
+		String vazio = " ";
+		String outroVazio = vazio.trim(); // Método trim().
+		System.out.println(outroVazio.isEmpty());
+		
+	}
+
+}
