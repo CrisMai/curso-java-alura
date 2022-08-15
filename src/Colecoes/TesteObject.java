3a187773bc0f7d6bebd5a63dfa77d8c80874413d
CrisMai <crismai.dev@gmail.com>
2022-08-15 15:49:12 -0300

    Arrays

---
+++ src/Colecoes/TesteObject.java
@@ -0,0 +1,40 @@
+package Colecoes;
+
+import bytebank.Cliente;
+import herdadoconta.Conta;
+import herdadoconta.ContaCorrente;
+import herdadoconta.ContaPoupanca;
+
+public class TesteObject {
+
+	public static void main(String[] args) {
+
+		System.out.println("X");
+		System.out.println(3);
+		System.out.println(true);
+
+		Object cc = new ContaCorrente(22, 33);
+		Object cp = new ContaPoupanca(33, 22);
+		Object cliente = new Cliente();
+
+		System.out.println(cc);
+		System.out.println(cp);
+
+		println(cliente);
+	}
+
+	static void println() {
+	}
+
+	static void println(int a) {
+	}
+
+	static void println(boolean valor) {
+	}
+
+	static void println(Object referencia) {
+	}
+
+}
+
+
