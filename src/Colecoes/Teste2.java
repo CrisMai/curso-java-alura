1f1af67569e81300cc47630763244090c8a72398
CrisMai <crismai.dev@gmail.com>
2022-08-17 15:53:54 -0300

    Generics

--- src/Colecoes/Teste2.java
+++ src/Colecoes/Teste2.java
@@ -2,49 +2,53 @@ package Colecoes;
 
 import java.util.ArrayList;
 
+import bytebank.Cliente;
 import herdadoconta.Conta;
 import herdadoconta.ContaCorrente;
 
 public class Teste2 {
-	
+
 	public static void main(String[] args) {
-		
-		ArrayList lista = new ArrayList();
-		
+
+		// Generics
+		ArrayList<Conta> lista = new ArrayList<Conta>();
+
 		Conta cc = new ContaCorrente(22, 11);
-        lista.add(cc);
-        Conta cc2 = new ContaCorrente(22, 22);
-        lista.add(cc2);
-        
-        System.out.println(lista.size());
-        
-        Conta ref = (Conta) lista.get(0);
-        
-        System.out.println(ref.getNumero());
-        
-        lista.remove(0);
-        System.out.println("Tamanho: " + lista.size());
-        
-        
-        Conta cc3 = new ContaCorrente(33, 311);
-        lista.add(cc3);
-        Conta cc4 = new ContaCorrente(33, 322);
-        lista.add(cc4);
-        
-        
-        for(int i = 0; i < lista.size(); i++) {
-        	Object oRef = lista.get(i);
-            System.out.println(oRef);
-        }
-        
-        
-        System.out.println("-------");
+		lista.add(cc);
 
-        for(Object oRef : lista) {
-            System.out.println(oRef);
-        }
+		Conta cc2 = new ContaCorrente(22, 22);
+		lista.add(cc2);
 
-		
+		System.out.println("Tamanho: " + lista.size());
+		Conta ref = lista.get(0);
+		System.out.println(ref.getNumero());
+
+		lista.remove(0);
+
+		System.out.println("Tamanho: " + lista.size());
+
+		Conta cc3 = new ContaCorrente(33, 311);
+		lista.add(cc3);
+		Conta cc4 = new ContaCorrente(33, 322);
+		lista.add(cc4);
+
+		// for(int i = 0; i < lista.size(); i++) {
+		// Object oRef = lista.get(i);
+		// System.out.println(oRef);
+		// }
+
+		System.out.println("-------");
+
+		for (Conta conta : lista) {
+			System.out.println(conta);
+		}
+
 	}
 
 }
+
+
+/* Estes símbolos que utilizamos (<>) se chamam generics. 
+ Temos uma classe, ela é genérica, mas ao utilizá-la, podemos 
+ tipificar o que ela conterá, por exemplo, String.
+ */
