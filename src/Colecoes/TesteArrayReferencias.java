9cc9dad0023093507b77319c16f54de0cb0cb67d
CrisMai <crismai.dev@gmail.com>
2022-08-17 14:02:18 -0300

    Array do tipo Object; Cast

--- src/Colecoes/TesteArrayReferencias.java
+++ src/Colecoes/TesteArrayReferencias.java
@@ -1,29 +1,40 @@
 package Colecoes;
 
+import bytebank.Cliente;
+import herdadoconta.Conta;
 import herdadoconta.ContaCorrente;
+import herdadoconta.ContaPoupanca;
 
 public class TesteArrayReferencias {
-	
+
 	public static void main(String[] args) {
-		
-		//int[] idades = new int[5];
-		
-        ContaCorrente[] contas = new ContaCorrente[5];
-        ContaCorrente cc1 = new ContaCorrente(22, 11);
-        contas[0] = cc1;
 
-        ContaCorrente cc2 = new ContaCorrente(22, 22);
-        contas[1] = cc2;
+		Object[] referencias = new Object[5];
 
-        //System.out.println(cc2.getNumero());
+		ContaCorrente cc1 = new ContaCorrente(22, 11);
+		referencias[0] = cc1;
 
-        System.out.println(contas[1].getNumero());
+		ContaPoupanca cc2 = new ContaPoupanca(22, 22);
+		referencias[1] = cc2;
 
-        ContaCorrente ref = contas[1];
-        
-        System.out.println(cc2.getNumero());
-        System.out.println(ref.getNumero());
-		
+		Cliente cliente = new Cliente();
+		referencias[2] = cliente;
+
+		// System.out.println(cc2.getNumero());
+
+		// Object referenciaGenerica = contas[1];
+
+		// System.out.println( referenciaGenerica.getNumero() );
+
+		ContaPoupanca ref = (ContaPoupanca) referencias[1]; // type cast
+		System.out.println(cc2.getNumero());
+		System.out.println(ref.getNumero());
+
 	}
 
 }
+
+/* Cast
+ * Transformamos uma referência de um tipo mais genérico, para uma de um tipo
+ * mais específico. Isso pode ser chamado também de type cast.
+ */
